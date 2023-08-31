6768a07eff3ee630a7a3cfd165d3a1d317f99bb1
-Taha Dostifam‍- <mr.tahadostifam@gmail.com>
2023-08-31 23:51:41 +0330

    NewChatButton done: (Scale animations on icons just like Telegram ;))

--- components/NewChatButton/NewChatButton.vue
+++ components/NewChatButton/NewChatButton.vue
@@ -1,32 +1,41 @@
 <script>
 import "./NewChatButton.scss";
 export default {
-    data() {
-        return {
-            isOpen: false,
-        };
-    },
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
 };
 </script>
 
 <template>
-  <v-menu class="custom-menu" transition="scale-transition" location="bottom end">
+  <v-menu class="custom-menu" transition="scale-transition" location="bottom end" v-model="isOpen">
     <template v-slot:activator="{ props }">
       <v-btn v-bind="props" class="new_chat_button rounded-circle" flat color="primary">
-        <v-icon v-if="props.isActive" icon="mdi-close"></v-icon>
-        <v-icon v-else icon="mdi-pen"></v-icon>
+        <v-scale-transition>
+          <v-icon v-if="isOpen" icon="mdi-close"></v-icon>
+          <v-icon v-else icon="mdi-pen"></v-icon>
+        </v-scale-transition>
       </v-btn>
     </template>
 
     <v-list>
       <v-list-item>
-        <v-list-item-title>New Channel</v-list-item-title>
+        <v-list-item-title>
+          <v-icon icon="mdi-bullhorn-outline"> </v-icon>
+          New Channel
+        </v-list-item-title>
       </v-list-item>
       <v-list-item>
-        <v-list-item-title>New Group</v-list-item-title>
+        <v-list-item-title>
+          <v-icon icon="mdi-account-multiple-outline"> </v-icon>
+          New Group</v-list-item-title>
       </v-list-item>
       <v-list-item>
-        <v-list-item-title>New Private Chat</v-list-item-title>
+        <v-list-item-title>
+          <v-icon icon="mdi-account-outline"> </v-icon>
+          New Private Chat</v-list-item-title>
       </v-list-item>
     </v-list>
   </v-menu>
